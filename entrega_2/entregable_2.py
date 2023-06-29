8d4aa7487abb0f5426e8312a682e875777c117bc
unknown <laureanoengulian@gmail.com>
2023-06-29 19:26:29 -0300

    Entrega 2

--- entrega_2/entregable_2.py
+++ entrega_2/entregable_2.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
-import redshift_connector
 from dotenv import dotenv_values
+import psycopg2
 
 env_vars = dotenv_values('.env')
 
@@ -28,9 +28,17 @@ for symbol in symbols:
     
     symbol_df['symbol'] = data['Meta Data']['2. Symbol']
 
+    symbol_df['open'] = pd.to_numeric(symbol_df['open'])
+    symbol_df['close'] = pd.to_numeric(symbol_df['close'])
+    symbol_df['avg'] = (symbol_df['open'] + symbol_df['close'])/2
+    symbol_df['pk'] = symbol_df['symbol']+symbol_df['week_from']
+
+    # symbol_df['avg'] = symbol_df[['close', 'open']].mean(axis=1)
+    
     df = pd.concat([df, symbol_df])
 
-conn = redshift_connector.connect(
+# Connect to Redshift using psycopg2
+conn = psycopg2.connect(
     host=env_vars['HOST'],
     port=int(env_vars['PORT']),
     database=env_vars['DATABASE'],
@@ -38,30 +46,52 @@ conn = redshift_connector.connect(
     password=env_vars['PASSWORD']
  )
 
-# Create a Cursor object
 cursor = conn.cursor()
 
-# Eliminate the table if exists
-cleaner_query = '''DROP TABLE IF EXISTS laureanoengulian_coderhouse.big_five_weekly;'''
-cursor.execute(cleaner_query)
+
+# https://docs.aws.amazon.com/redshift/latest/dg/merge-replacing-existing-rows.html
+sql_transaction = ''' begin transaction;
+                      
+                      CREATE TABLE IF NOT EXISTS laureanoengulian_coderhouse.big_five_weekly(
+                      "week_from" varchar(256) not null,
+                      "open" float8 not null,
+                      "high" varchar(256) not null,
+                      "low" varchar(256) not null,
+                      "close" float8 not null,
+                      "volume" varchar(256) not null,
+                      "symbol" varchar(256) not null,
+                      "avg" float8 not null,
+                      "pk" varchar(256));
+
+
+                      delete from laureanoengulian_coderhouse.big_five_weekly using laureanoengulian_coderhouse.stage 
+                      where big_five_weekly.pk = stage.pk;
+
+                      insert into laureanoengulian_coderhouse.big_five_weekly
+                      select * from laureanoengulian_coderhouse.stage;
+
+                      end transaction;
+                  '''
+
+cursor.execute(sql_transaction)
 conn.commit()
 
-# Create table if not exists
-create_table_query = '''CREATE TABLE IF NOT EXISTS laureanoengulian_coderhouse.big_five_weekly(
-                        "week_from" date not null,
-                        "open" decimal(38, 4) not null,
-                        "high" decimal(38, 4) not null,
-                        "low" decimal(38, 4) not null,
-                        "close" decimal(38, 4) not null,
-                        "volume" bigint not null,
-                        "symbol" varchar(15) not null);
-                     '''
-cursor.execute(create_table_query)
+drop_tmp = '''drop table if exists laureanoengulian_coderhouse.stage;'''
+
+cursor.execute(drop_tmp)
 conn.commit()
 
-# Load the df in Redshift
-cursor.write_dataframe(df=df, table='big_five_weekly')
-conn.commit()
+#Chequeo de valores únicos
+cursor = conn.cursor()
+cursor.execute(f"""
+SELECT
+  count(pk), count(distinct pk)
+FROM
+  laureanoengulian_coderhouse.big_five_weekly;
+""")
+# resultado = cursor.fetchall()
+print(", ".join(map(lambda x: str(x), cursor.fetchall())))
+cursor.close()
 
 
 
